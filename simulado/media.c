b28e515a947c3d43c7b1837331895f24805634ac
LEANDRO RODRIGUES <leandrorodriguesdasilvalima@gmail.com>
2025-01-22 22:55:42 -0300

    vai da certo

---
+++ simulado/media.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    float nota1,nota2,nota3;
+    float media;
+
+    printf("Digite sua primeira nota?");
+    scanf("%f", &nota1);
+    printf("Digite sua segunda nota?");
+    scanf("%f", &nota2);
+    printf("Digite sua terceira nota?");
+    scanf("%f", &nota3);
+
+    media= (nota1 + nota2 + nota3) /3;
+
+    printf("A media é %.2f",media);
+
+    return 0;
+
+
+}
