92b43083272ad75ab1954a8b098198e0ec368aa6
LEANDRO RODRIGUES <leandrorodriguesdasilvalima@gmail.com>
2025-01-22 22:52:18 -0300

    novo

---
+++ novoteste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char nome[50];
+    int idade;
+    float altura;
+
+    printf("Qual seu nome?");
+    scanf("%s",&nome);
+    printf("QUal sua idade?");
+    scanf("%d",&idade);
+    printf("Qual sua altura?");
+    scanf("%f",&altura);
+    
+
+}
