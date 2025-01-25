7df585f60d75e21802ccf2dda086c58fd5757b80
LEANDRO RODRIGUES <leandrorodriguesdasilvalima@gmail.com>
2025-01-24 22:55:26 -0300

    atual

---
+++ simulado/jogoPc.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    int numeroJogador,numeroPc,resultado;
+    char tipodeComparacao;
+
+
+    srand(time(0));
+    numeroPc = rand() % 100 + 1;
+
+    printf("Bem vindo ao Jogo de Numeros Aleatorios!\n");
+    printf("Para o jogo entre nos numeros maiores digite (M):\n");
+    printf("Para Jogar quem acha o numero menor digite (N):\n");
+    printf("Para Jogar ,achar um numero igual digite (I):\n");
+    printf("Digite sair (S):\n");
+   
+    printf("Digite sua opção:");
+    scanf("%s",&tipodeComparacao);
+
+    printf("Digite seu numero entre 1 e 100:");
+    scanf("%d",&numeroJogador);
+
+    switch (tipodeComparacao)
+    {
+    case 'M':
+    case 'm':
+        printf("Você escolheu o Jogo Maior Numero!\n");
+        
+        resultado = numeroJogador>numeroPc ? 1:0;
+
+        break;
+    case 'N':
+    case 'n':
+        printf("Você escolheu o jogo do menor numero!\n");
+        resultado= numeroJogador<numeroPc ? 1:0;
+        break;
+
+    case 'I':
+    case 'i':
+        printf("Você escolheu o Jogo do Igual!\n");
+        resultado=numeroJogador==numeroPc ? 1:0;
+        break;
+    
+    default:
+        printf("Opção Invalida!");
+        break;
+    }
+    
+    
+    printf("O seu numero é %d: e o numero do Computador é %d:\n",numeroJogador,numeroPc);
+
+    if (resultado==1)
+    {
+        printf(" Parabéns você ganhou o jogo!");   
+    }
+    else{
+    printf(" Infelimente não foi dessa vez!");
+    }
+    return 0;
+}
