1080f04f93aa6237aa55cc2470953e65ae3a4db7
LEANDRO RODRIGUES <leandrorodriguesdasilvalima@gmail.com>
2025-01-24 23:58:38 -0300

    atual2

--- simulado/jogoPc.c
+++ simulado/jogoPc.c
@@ -5,8 +5,9 @@ int main(){
     int numeroJogador,numeroPc,resultado;
     char tipodeComparacao;
 
-
+//srand veio da declaração da função stdlib
     srand(time(0));
+//  AQUI O NUMERO DO ALEATORIO E OBTIDO SOMANDO MAIS UM PQ A DIVISAO DE 100 NUMERO DARIA SO 99
     numeroPc = rand() % 100 + 1;
 
     printf("Bem vindo ao Jogo de Numeros Aleatorios!\n");
@@ -14,13 +15,16 @@ int main(){
     printf("Para Jogar quem acha o numero menor digite (N):\n");
     printf("Para Jogar ,achar um numero igual digite (I):\n");
     printf("Digite sair (S):\n");
-   
+
+    //OPÇÃO PARA LEVAR AO MENU SWITCH   
     printf("Digite sua opção:");
     scanf("%s",&tipodeComparacao);
-
+    //PERGUTA AO OPERADOR PRA SABER SEU NUMERO
     printf("Digite seu numero entre 1 e 100:");
     scanf("%d",&numeroJogador);
 
+
+//NO SWITCH A DECLARAÇÃO COMO É UMA STHING TEM QUE ESTA EM ASPAS SIMPLES E A OPÇÃO MINUSCULA É PARA NAO HAVER ERROS CASO O OPERADOR ERRE
     switch (tipodeComparacao)
     {
     case 'M':
@@ -46,6 +50,8 @@ int main(){
         printf("Opção Invalida!");
         break;
     }
+
+    //AO FINAL É DECLADO OS NUMEROS DO OPERADOR E DO COMPUTADOR E O RESULTADO É COMPARADO POR O IF E ELSE
     
     
     printf("O seu numero é %d: e o numero do Computador é %d:\n",numeroJogador,numeroPc);
